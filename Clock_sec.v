d1ddf29b8ab10b165eb808864eb18367a75c526f
Snooze <2020285047@email.szu.edu.cn>
2022-11-20 18:10:31 +0800

    [fix] Clock_sec 输出输入端弄反

--- Clock_sec.v
+++ Clock_sec.v
@@ -1,9 +1,9 @@
 module Clock_sec (
-    clk_in_50M,
-    clk_out_sec
+    clk_out_sec,
+    clk_in_50M
 );
-output clk_in_50M;      // 50MHz -> 20ns
-input clk_out_sec;      // 输出 1Hz -> 1s
+output clk_out_sec;      // 输出 1Hz -> 1s
+input clk_in_50M;      // 50MHz -> 20ns
 wire clk1, clk2, clk3;
 Div_100(clk1, clk_in_50M);
 Div_100(clk2, clk1);
