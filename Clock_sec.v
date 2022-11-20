ce38c0c69f3ca338237a602cd69b8e30125b2290
Snooze <2020285047@email.szu.edu.cn>
2022-11-20 18:18:42 +0800

    [feat] Clock_sec 设置多种快进秒数生成代码注释

--- Clock_sec.v
+++ Clock_sec.v
@@ -4,10 +4,27 @@ module Clock_sec (
 );
 output clk_out_sec;      // 输出 1Hz -> 1s
 input clk_in_50M;      // 50MHz -> 20ns
-wire clk1, clk2, clk3;
-Div_100(clk1, clk_in_50M);
-Div_100(clk2, clk1);
-Div_100(clk3, clk2);
-Div_5(clk_out_sec, clk3);
+
+// 根据需要解开相应注释
+// 标准一秒
+// wire clk1, clk2, clk3;
+// Div_100(clk1, clk_in_50M);
+// Div_100(clk2, clk1);
+// Div_100(clk3, clk2);
+// Div_5(clk_out_sec, clk3);
+
+// 加快100倍
+// wire clk1, clk2;
+// Div_100(clk1, clk_in_50M);
+// Div_100(clk2, clk1);
+// Div_5(clk_out_sec, clk2);
+
+// 加快500倍
+// wire clk1;
+// Div_100(clk1, clk_in_50M);
+// Div_100(clk_out_sec, clk1);
+
+// 加快50000倍
+Div_100(clk_out_sec, clk_in_50M);
 
 endmodule //Clock_sec
