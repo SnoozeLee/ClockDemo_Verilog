be13dd2b3fa1476bf6fa0206caca08d4ab54e175
Snooze <2020285047@email.szu.edu.cn>
2022-12-12 11:18:28 +0800

    [fix] 修复数码管'2'显示不正确的问题

--- src/Clock_sec.v
+++ src/Clock_sec.v
@@ -7,11 +7,11 @@ input clk_in_50M;      // 50MHz -> 20ns
 
 // 根据需要解开相应注释
 // 标准一秒
-// wire clk1, clk2, clk3;
-// Div_100(clk1, clk_in_50M);
-// Div_100(clk2, clk1);
-// Div_100(clk3, clk2);
-// Div_5(clk_out_sec, clk3);
+//  wire clk1, clk2, clk3;
+//  Div_100 div_A(clk1, clk_in_50M);
+//  Div_100 div_B(clk2, clk1);
+//  Div_100 div_C(clk3, clk2);
+//  Div_5 divD(clk_out_sec, clk3);
 
 // 加快100倍
 // wire clk1, clk2;
